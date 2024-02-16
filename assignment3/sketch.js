42f276a6977719456b6095d97a036ee547c3630e
SDowling4 <157869484+SDowling4@users.noreply.github.com>
2024-02-15 22:58:04 -0600

    Update sketch.js

--- assignment3/sketch.js
+++ assignment3/sketch.js
@@ -8,7 +8,7 @@ stand: {row:0, frames:1},
 walkRight: {row:0, col:1,frames:8}
 };
 characters.push(new Character(200,100,80,80,'assets/robot.png',animations));
-characters.push(new Character(200,200,80,80,'ssets/guy.png',animations));
+characters.push(new Character(200,200,80,80,'assets/guy.png',animations));
 characters.push(new Character(200,300,80,80,'assets/green.png',animations));
 }
 
